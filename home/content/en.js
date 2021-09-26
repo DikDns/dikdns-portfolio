04151efbe9124b93b2e73f76d16c7597849af4fc
DikDns <dikdnssocial@gmail.com>
2021-09-26 18:51:29 +0700

    change to hiatus and offline content

--- home/content/en.js
+++ home/content/en.js
@@ -1,4 +1,57 @@
+const nav = `<!-- NAV SECTION -->
+<nav id="nav" class="nav">
 
+        <div class="nav-box">
+            <div class="nav-header">
+                <h2>DikDns</h2>
+            </div>
+
+            <div class="nav-lang">
+                <div class="lang-dropdown">
+                    <button class="lang-button"><i class="fas fa-globe"></i> En <i class="fas fa-caret-right arrow-icon"></i></button>
+                    <div class="list">
+                        <a id="lang" class="list-item" href="#">Bahasa Indonesia</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="nav-link">
+            <ul>
+                <li class="link">
+                    <a href="../">
+                        <i class="icons fas fa-house-user"></i>
+                        <p class="link-desc">Home</p>
+                    </a>
+                </li>
+                <li class="link">
+                    <a href="/about">
+                        <i class="icons fas fa-info-circle"></i>
+                        <p class="link-desc">About</p>
+                    </a>
+                </li>
+                <li  class="link">
+                    <a href="/suggestion">
+                        <i class="icons fas fa-comment-alt"></i>
+                        <p class="link-desc">Suggestion</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="nav-menu-toggle">
+            <input class="btn-toggle" type="checkbox">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+    </nav>
+`;
+
+const main = `<!-- MAIN SECTION -->
+
+`;
 
 
 
